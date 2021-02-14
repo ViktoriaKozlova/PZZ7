139a3cde684d46ae69eca5326a59db3979c4d7b7
ViktoriaKozlova <73911785+ViktoriaKozlova@users.noreply.github.com>
2021-02-14 14:10:17 +0200

    Add files via upload

---
+++ head.load.js
@@ -0,0 +1,20 @@
+(function (win, undefined) {
+    "use strict";
+
+    //#region variables
+    var doc        = win.document,
+        domWaiters = [],
+        handlers   = {}, // user functions waiting for events
+        assets     = {}, // loadable items in various states
+        isAsync    = "async" in doc.createElement("script") || "MozAppearance" in doc.documentElement.style || win.opera,
+        isDomReady,
+
+        /*** public API ***/
+        headVar = win.head_conf && win.head_conf.head || "head",
+        api     = win[headVar] = (win[headVar] || function () { api.ready.apply(null, arguments); }),
+
+        // states
+        PRELOADING = 1,
+        PRELOADED  = 2,
+        LOADING    = 3,
+        LOADED     = 4;
